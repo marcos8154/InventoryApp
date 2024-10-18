956b3eb616f6ca20adf40761400addc24445900b
Automac <Automac@JULIO-PC>
2024-10-18 17:54:36 -0300

    First commit

---
+++ Inventory2/MainPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using Inventory2.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Inventory2
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+      
+        }
+
+        private void FillMenu()
+        {
+            ListMenu.ItemsSource = new List<object>() {
+                new AppMenuItem(
+                    id : 0,
+                    icon : "products_blue.png",
+                    title : "Manage Products",
+                    subtitle : "Import and View Products"),
+                new AppMenuItem(icon : "expeditions_blue.png",
+                    id : 1,
+                    title : "Expeditions",
+                    subtitle : "Separate products"),
+                new AppMenuItem(icon : "inventory_blue.png",
+                    id : 2,
+                    title : "Inventory",
+                    subtitle : "Update the actual quantities of the product")
+            };
+        }
+
+        private void ListMenu_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            AppMenuItem item = (AppMenuItem)ListMenu.SelectedItem;
+            if (item.Id == 0)
+                Navigation.PushAsync( new ManageProducts());
+        }
+
+        private void ContentPage_Appearing(object sender, EventArgs e)
+        {
+            FillMenu();
+        }
+    }
+}
---
+++ Inventory2/Models/AppMenuItem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inventory2.Models
+{
+    internal class AppMenuItem
+    {
+        public AppMenuItem(string icon, string title, string subtitle, int id)
+        {
+            Icon = icon;
+            Title = title;
+            Subtitle = subtitle;
+            Id = id;
+        }
+
+        public String Icon { get; set; }
+        public String Title { get; set; }
+        public String Subtitle { get; set; }
+        public int Id { get; internal set; }
+    }
+}
